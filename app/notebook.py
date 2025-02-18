ce6c61144543ce10a64498604b8ddd59e966cffa
Juan Pablo <Samus2978@gmail.com>
2025-02-18 10:48:41 -0500

    Initial comitjp

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,8 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+class Note:
+    def __init__(self, HIGH=None, MEDIUM=None, LOW= None):
+        self.HIGH:str = HIGH
+        self.MEDIUM:str = MEDIUM
+        self.LOW:str = LOW
+
