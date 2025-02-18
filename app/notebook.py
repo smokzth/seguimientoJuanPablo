bbc2308d0cc25efc14d6c6a4ed17a97ab8cfb7b0
Juan Pablo <francopablo155@gmail.com>
2025-02-18 11:10:49 -0500

    Second commit def tags

--- app/notebook.py
+++ app/notebook.py
@@ -1,8 +1,24 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
-
+from datetime import datetime
 class Note:
-    def __init__(self, HIGH=None, MEDIUM=None, LOW= None):
-        self.HIGH:str = HIGH
-        self.MEDIUM:str = MEDIUM
-        self.LOW:str = LOW
+    self.HIGH: str = HIGH
+    self.MEDIUM: str = MEDIUM
+    self.LOW: str = LOW
+
+
+    def __init__(self, code: str, title: str, text: str, importance: str ):
+        self.code: str = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: datetime.now()
+        self.tags :list[str] =[]
+
+    def add_tags(self,tag: str):
+
+
+class Notebook:
+    def __init__(self, ):
+
+
 
