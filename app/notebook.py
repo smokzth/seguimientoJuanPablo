a8bf424e5764aed94aed12a9aa19c1a93ed5702e
Juan Pablo Gaviria <francopablo155@gmail.com>
2026-02-16 17:16:52 -0500

    commit final clase

--- app/notebook.py
+++ app/notebook.py
@@ -14,7 +14,8 @@ class Note:
         self.tags: list[str] = []
 
     def add_tags(self, tag: list[str]):
-        self.tags.append(tag)
+        if tag not in self.tags:
+            self.tags.append(tag)
 
     def __str__(self) -> str:
         return (f"date{self.creation_date}"
@@ -26,6 +27,36 @@ class Notebook:
         self.notes: list[Note] = notes
 
     def add_note(self, title: str, text: str, importance: str) -> int:
+        new_code = len(self.notes) + 1
+        new_note = Note(new_code, title, text, importance)
+        self.notes.append(new_note)
+        return new_code
+
+    def delete_note(self, code: int):
+        for note in self.notes:
+            if note.code == code:
+                self.notes.remove(note)
+                return
+
+    def important_notes(self) -> list[Note]:
+        result = []
+        for note in self.notes:
+            if note.importance == Note.HIGH or note.importance ==  Note.MEDIUM:
+                result.append(note)
+            return result
+
+    def notes_by_tag(self, tag: str) -> list[Note]:
+        result = []
+        for note in self.notes:
+            for t in note.tags:
+                if t == tag:
+                    result.append(note)
+        return result
+
+
+    def tag_with_most_notes(self):
+        pass
+
 
 
 
