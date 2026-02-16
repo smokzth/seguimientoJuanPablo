ea698e877a6b6b947b7961691e5f3fbbe20f0bae
Juan Pablo Gaviria <francopablo155@gmail.com>
2026-02-16 16:31:53 -0500

    commit inicial, atributos del constructor

--- app/notebook.py
+++ app/notebook.py
@@ -1,26 +1,20 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
 from datetime import datetime
 class Note:
-    HIGH: str = "HIGH"
-    MEDIUM: str = "MEDIUM"
-    LOW: str = "LOW"
-    def __init__(self, code: str, title: str, text: str, importance: str):
+    HIGH : str  = "HIGH"
+    MEDIUM : str = "MEDIUM"
+    LOW : str = "LOW"
+
+    def __int__(self, code: str, title: str, text: str, importance: str, creation_date: datetime ):
         self.code: str = code
         self.title: str = title
         self.text: str = text
         self.importance: str = importance
-        self.creation_date: datetime.now()
-        self.tags =[]
+        self.creation_date: datetime = datetime.now()
 
-    def add_tag(self,tag: str):
-        if tag not in self.tags:
-            self.tags.append(tag)
 
-    def __str__(self):
-        return (f"Date : {self.creation_date}\n"
-                f"{self.title}:{self.text}"
-class Notebook:
-    def __init__(self)
+
+
 
 
 
